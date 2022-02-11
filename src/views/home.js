067b38ed02609c4afeed8795ee66eb15981b196f
Kallen Hager <98243303+kallenhager@users.noreply.github.com>
2022-02-10 20:08:25 -0500

    Commit from TeeportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -17,7 +17,7 @@ const Home = () => {
           <div className={styles['container1']}>
             <img
               alt="image"
-              src="/playground_assets/0kallen-1500h.jpg"
+              src="/playground_assets/0kallen-300h.jpg"
               className={styles['image']}
             />
             <div className={styles['container2']}>
@@ -25,7 +25,9 @@ const Home = () => {
                 <span>Hi, I&apos;m Kallen</span>
               </span>
               <div className={styles['container3']}>
-                <span className={projectStyles['headingText']}>
+                <span
+                  className={` ${styles['text02']} ${projectStyles['headingText']} `}
+                >
                   <span>
                     I&apos;m an aspiring data scientist with interests in
                     consumer behavior, emotional processing, and health. Last
@@ -53,9 +55,9 @@ const Home = () => {
           <div className={styles['Format-Card']}>
             <div className={styles['Text-Container']}>
               <span
-                className={` ${styles['Text08']} ${projectStyles['card-Heading']} `}
+                className={` ${styles['Title']} ${projectStyles['card-Heading']} `}
               >
-                <span>Metacritic Scrape</span>
+                <span>Metacritic Scrape &amp; ANalysis</span>
               </span>
               <span className={projectStyles['card-Text']}>
                 <span>
@@ -76,12 +78,16 @@ const Home = () => {
                   Which critic&apos;s scores correlate best with sales?
                 </span>
               </span>
-              <span className={styles['text21']}>
-                <span>Github</span>
-                <span>&amp;#8203;</span>
-                <br></br>
-                <span></span>
-              </span>
+              <div className={styles['container4']}>
+                <a
+                  href="https://github.com/kallenhager/metacritic_scraper"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className={` ${styles['Link']} ${projectStyles['link']} ${projectStyles['link']} `}
+                >
+                  <span className={styles['text20']}>Github</span>
+                </a>
+              </div>
             </div>
             <div className={styles['Image1']}>
               <img
@@ -93,28 +99,100 @@ const Home = () => {
           </div>
         </div>
         <div
+          id="erp"
           className={` ${styles['White-Card2']} ${projectStyles['projectbox']} `}
         >
           <div className={styles['Format-Card1']}>
             <div className={styles['Text-Container1']}>
               <span
-                className={` ${styles['Text26']} ${projectStyles['card-Heading']} `}
+                className={` ${styles['Title1']} ${projectStyles['card-Heading']} `}
               >
-                <span>Metacritic Scrape</span>
+                <span>
+                  Implicit and Explicit Attitudes Toward LGBT Individuals â An
+                  ERP Examination Using Responses to Emotional Images and Reward
+                </span>
               </span>
               <span className={projectStyles['card-Text']}>
                 <span>
-                  Scraping data to answer the following questions for PC games:
-                  Should a videogame publisher/developer pay attention to critic
-                  reviews? Which critic&apos;s scores correlate best with sales?
+                  This is my undergraduate thesis project. I cleaned up some
+                  data in from an incomplete EEG experiment on homonegativity,
+                  prejudice, and sexual imagery. I used R and SPSS to examine
+                  correlations between valence rated images and proposed
+                  implicit measures of sexual prejudice
                 </span>
               </span>
+              <div className={styles['container5']}>
+                <a
+                  href="https://docs.google.com/document/d/1vtWn-o7sblhUrK1HUBInWz8Kg9bGAFtm/edit?usp=sharing&amp;ouid=106166205784524408384&amp;rtpof=true&amp;sd=true"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className={styles['Link1']}
+                >
+                  GooGle DoCs
+                </a>
+              </div>
             </div>
             <div className={styles['Image3']}>
               <img
                 alt="image"
-                src="/playground_assets/scraper-1200w.png"
+                src="/playground_assets/screenshot%202022-02-10%20190249-1200w.png"
                 className={styles['image4']}
+              />
+            </div>
+          </div>
+        </div>
+        <div
+          id="cannabinoids"
+          className={` ${styles['White-Card3']} ${projectStyles['projectbox']} `}
+        >
+          <div className={styles['Format-Card2']}>
+            <div className={styles['Text-Container2']}>
+              <span
+                className={` ${styles['Title2']} ${projectStyles['card-Heading']} `}
+              >
+                <span>
+                  Review of the therapeutic potential of selected minor
+                  cannabinoids
+                </span>
+              </span>
+              <span className={styles['text25']}>
+                <span>
+                  With the passage of 2018 Agriculture Improvement Act the
+                  United States legalized the all cannabis-derived cannabinoids
+                  apart from Î-9-THC. These minor cannabinoids are now being
+                  sold and utilized in concentrations higher than ever seen
+                  before in whole-plant preparations. Minor cannabinoid products
+                  currently available to the public demonstrate considerable
+                  therapeutic potential relevant to a wide variety of
+                  conditions. As a class of compounds, they share many
+                  therapeutic effects (anti-inflammatory, anti-epileptic).
+                  However, it is clear that different cannabinoids possess
+                  relatively unique effects, and that some of these effects are
+                  in direct opposition to one another (hyperphagia, hypophagia).
+                  Differential affinities for and actions at multiple receptors,
+                  namely CB1 and CB2, seem to be responsible for these
+                  contradictory effects. While the evidence for the therapeutic
+                  potential of these minor cannabinoids is promising, more
+                  research is needed before any of these products can be
+                  confidently recommended by clinicians.
+                </span>
+              </span>
+              <div className={styles['container6']}>
+                <a
+                  href="https://1drv.ms/w/s!Ai843pWZk8qtgfpOsdHPa2Nm0m-ezA?e=0UVF17"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className={` ${styles['Link2']} ${projectStyles['link']} `}
+                >
+                  WORD ONLINE
+                </a>
+              </div>
+            </div>
+            <div className={styles['Image5']}>
+              <img
+                alt="image"
+                src="/playground_assets/screenshot%202022-02-10%20200121-1200w.png"
+                className={styles['image6']}
               />
             </div>
           </div>
@@ -122,23 +200,25 @@ const Home = () => {
       </div>
       <div className={styles['Sidebar']}>
         <nav className={styles['Nav']}>
-          <div className={styles['container4']}>
+          <div className={styles['container7']}>
             <a
               href="#metacritic"
-              className={` ${styles['link']} ${projectStyles['navbar-Item']} ${projectStyles['navitem']} `}
+              className={` ${projectStyles['navbar-Item']} ${projectStyles['navitem']} ${projectStyles['navbar-Item']} `}
             >
               METACRITIC SCRAPE &amp; ANALYIS
             </a>
-            <span
-              className={` ${styles['text30']} ${projectStyles['navbar-Item']} ${projectStyles['navitem']} ${projectStyles['navbar-Item']} `}
+            <a
+              href="#erp"
+              className={` ${styles['link4']} ${projectStyles['navbar-Item']} ${projectStyles['navitem']} ${projectStyles['navbar-Item']} `}
             >
               IMPLICIT ATTITUDES ERP EXAMINATION
-            </span>
-            <span
-              className={` ${projectStyles['navbar-Item']} ${projectStyles['navitem']} ${projectStyles['navbar-Item']} `}
+            </a>
+            <a
+              href="#cannabinoids"
+              className={` ${styles['link5']} ${projectStyles['navbar-Item']} ${projectStyles['navitem']} ${projectStyles['navbar-Item']} `}
             >
               MINOR CANNABINIODS REVIEW
-            </span>
+            </a>
           </div>
         </nav>
       </div>
